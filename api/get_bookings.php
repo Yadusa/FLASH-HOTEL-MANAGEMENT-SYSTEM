620212dc0b46ba5f20809632588cbbacd575931d
Yadusha21 <1221205086@soffice.mmu.edu.my>
2025-12-01 00:51:03 +0800

    php

--- api/get_bookings.php
+++ api/get_bookings.php
@@ -1,28 +1,12 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+include 'db.php';
 
+$result = $conn->query("SELECT * FROM bookings");
+$data = [];
 
-$servername = "localhost";
-$username = "root";
-$password = ""; 
-$dbname = "flashhotel";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    echo json_encode(["error" => "Database connection failed"]);
-    exit();
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
 }
 
-$sql = "SELECT * FROM bookings ORDER BY id DESC";
-$result = $conn->query($sql);
-
-$bookings = [];
-while($row = $result->fetch_assoc()) {
-    $bookings[] = $row;
-}
-
-echo json_encode($bookings);
-$conn->close();
+echo json_encode($data);
 ?>
