3e473a704394948f07f3cb9b897b30c68775fad1
Yadusha21 <1221205086@soffice.mmu.edu.my>
2025-12-22 17:22:44 +0800

    user edit

--- user_login.php
+++ user_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "config/db.php";
+require_once "db.php";
 
 $username = $_POST['username'];
 $password = $_POST['password'];
--- user_register.php
+++ user_register.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once "config/db.php";
+require_once "db.php";
 
 $username = $_POST['username'];
 $cust_name = $_POST['cust_name'];
